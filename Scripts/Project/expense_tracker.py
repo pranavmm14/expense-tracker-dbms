9dcddb8de0022b8116abcaf1e1d42927bed77e3b
pranav-new-laptop <pranavmehe14@gmail.com>
2023-10-17 17:46:44 +0530

    Added gitignore

--- Scripts/Project/expense_tracker.py
+++ Scripts/Project/expense_tracker.py
@@ -1,3 +1,15 @@
 import mysql.connector
+import json
+
+with open('config.json','r') as c:
+    params= json.load(c)['params']
 
 print("Import success!")
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="spmm"
+)
+
+print(mydb)
