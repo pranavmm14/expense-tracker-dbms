031997ef2a5cdefe9fbbd8cafd4328e996aefed4
Rohit <your.email@example.com>
2023-10-22 21:50:53 +0530

    added the frames and adjest the frames

--- Scripts/Project/home_page.py
+++ Scripts/Project/home_page.py
@@ -1,112 +1,99 @@
-from tkinter import PhotoImage 
+from tkinter import PhotoImage
 from tkinter import *
 
 # Create the main window
 window = Tk()
 window.title('Expense Tracker')
 window.config(bg='#0B5A81')
-window.resizable(False,False)
+window.resizable(False, False)
 window.geometry("1200x700+100+50")
-# background_image = PhotoImage(file="Scripts\\Project\\1.png")
-# label6 = Label(window, image = background_image)
-# label6.pack()
-
 
 f = ('Times', 14)
 var = StringVar()
 var.set('male')
 
+# Create a bottom frame at the bottom of the main window
+bottom_frame = Frame(window, width=1200, height=100, bg="#fff")
+bottom_frame.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky='s')
 
+label3 = Label(bottom_frame)
+label3.place(x=600, y=50, anchor="center")
 
+label4 = Label(label3, text="Total Amount")
+label5 = Label(label3, text="Current Amount")
 
-# Create a bottom frame below the top frames
-bottom_frame = Frame(window, width=900, height=100, bg="green")
-bottom_frame.grid(row=10, column=10, columnspan=2, padx=10, pady=10)
+register_TotalAmount = Entry(
+    label4,
+    font=f
+)
 
-# Add widgets to the frames
-right_frame = Frame(window, width=300, height=600, bg="red")
-right_frame.grid(row=3, column=10, padx=1, pady=1)
-window, 
-bd=2, 
-bg='#CCCCCC',
-relief=SOLID, 
-padx=10, 
-pady=10
-    
+register_CurrentAmount = Entry(
+    label5,
+    font=f
+)
+
+label4.grid(row=0, column=0, pady=10, padx=20)
+label5.grid(row=0, column=1, pady=10, padx=20)
+
+# Create a right frame on the right side
+right_frame = Frame(window, width=300, height=600, bg="#fff")
+right_frame.grid(row=0, column=2, padx=10, pady=10, sticky='ne')
+
 Label(
     right_frame, 
-    text="Enter Item", 
+    text="", 
     bg='#CCCCCC',
     font=f
-    ).grid(row=1, column=0, sticky=W, pady=10)
+).grid(row=0, column=0, sticky=W, pady=10)
+
+register_blank = Entry(
+    right_frame, 
+    font=f
+)
+register_blank.grid(row=0, column=1, pady=10, padx=20) 
+
+# Create a left frame on the left side
+left_frame = Frame(window, width=400, height=600, bg="#fff")
+left_frame.grid(row=0, column=0, padx=10, pady=10, sticky='nw')
+Label(
+    left_frame, 
+    text="Enter Item", 
+    bg='#fff',
+    font=f,
+    background='#fff',
+).grid(row=0, column=0, sticky=W, pady=10)
 
 Label(
-    right_frame, 
+    left_frame, 
     text="Add Amount", 
     bg='#CCCCCC',
-    font=f
-    ).grid(row=2, column=0, sticky=W, pady=10)
+    font=f,
+    background='#fff',
+).grid(row=1, column=0, sticky=W, pady=10)
 
-register_Itme = Entry(
-    right_frame, 
+register_Item = Entry(
+    left_frame, 
     font=f
-    )
+)
 
 register_Amount = Entry(
-    right_frame, 
+    left_frame, 
     font=f
-    )
+)
 
 register_btn = Button(
-    right_frame, 
+    left_frame, 
     width=15, 
     text='Add', 
     font=f, 
     relief=SOLID,
     cursor='hand2',
-    command=None
+    command=None,
 )
 
-register_Itme.grid(row=1, column=1, pady=10, padx=20) 
-register_Amount.grid(row=2, column=1, pady=10, padx=20)
-register_btn.grid(row=7, column=1, pady=10, padx=20)
-
-
-left_frame= Frame(window, width=300, height=600, bg="blue")
-left_frame.grid(row=3, column=20, padx=1, pady=1)
-window, 
-bd=2, 
-bg='#CCCCCC',
-relief=SOLID, 
-padx=10, 
-pady=10
-    
-Label(
-    left_frame, 
-    text="", 
-    bg='#CCCCCC',
-    font=f
-    ).grid(row=1, column=0, sticky=W, pady=10)
-
-register_blanck = Entry(
-    left_frame, 
-    font=f
-    )
-register_blanck.grid(row=1, column=1, pady=10, padx=20) 
-
-
-
-label3 = Label(bottom_frame, text="Frame 3 (Green)")
-label3.place(x=700, y=50, anchor="sw")
-
-# Create two Label widgets
-label4 = Label(label3, text="Column 1")
-label5 = Label(label3, text="Column 2")
-
-# Place the Label widgets in the label3 frame
-label4.grid(row=0, column=0)
-label5.grid(row=0, column=1)
-
+register_Item.grid(row=0, column=1, pady=10, padx=20) 
+register_Amount.grid(row=1, column=1, pady=10, padx=20)
+register_btn.grid(row=2, column=0, columnspan=2, pady=10)
 
 
 window.mainloop()
